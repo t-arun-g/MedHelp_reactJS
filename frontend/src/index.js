2cbdad9ad10b40bd70038474798e98cd46059b47
Srushhhti <ssrushti108@gmail.com>
2023-04-11 16:24:20 +0530

    corrected the style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div style="color:hello;background-color:blue" class="hello"></div>
     <App />
   </React.StrictMode>
 );
